89896a8774dd792bb93e35b34c62feb9f3a575f1
itzC9 <154809425+itzC9@users.noreply.github.com>
2025-06-25 00:35:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,42 @@
 function toggleMenu() {
   const menu = document.getElementById('sideMenu');
   if (menu.style.right === '0px') {
-    menu.style.right = '-250px';
+    menu.style.right = '-300px';
   } else {
     menu.style.right = '0px';
   }
 }
 
-// Future feature: fetch json to populate menu lists
-// fetch('https://yourgithubusername.github.io/yourrepo/mods.json')
-//   .then(res => res.json())
-//   .then(data => console.log(data));
+document.addEventListener('DOMContentLoaded', () => {
+  const execList = document.getElementById('executor-list');
+  const modList = document.getElementById('modmenus-list');
+
+  if (execList) {
+    fetch('executors.json')
+      .then(res => res.json())
+      .then(data => {
+        let html = '';
+        for (const platform in data) {
+          html += `<h3>${platform}</h3><ul>`;
+          for (const name in data[platform]) {
+            html += `<li><a href="${data[platform][name].link}" target="_blank">${name}</a></li>`;
+          }
+          html += '</ul>';
+        }
+        execList.innerHTML = html;
+      });
+  }
+
+  if (modList) {
+    fetch('modmenus.json')
+      .then(res => res.json())
+      .then(data => {
+        let html = '<ul>';
+        for (const name in data) {
+          html += `<li><a href="${data[name].link}" target="_blank">${name}</a></li>`;
+        }
+        html += '</ul>';
+        modList.innerHTML = html;
+      });
+  }
+});
